24ffdfbfae839c16ac3690bd0b6d360321cb00e2
devpop <impopcity@gmail.com>
2022-10-06 14:00:57 +0530

    usememo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useMemo, useCallback } from 'react';
 
-const App = () => {
-    const [userInput, setUserInput] = useState('');
-    const [result, setResult] = useState(0);
-    const [num1] = useState(4);
-    const [num2] = useState(5);
-
-    //const sum = useCallback(() => num1 + num2, [num1, num2]);
-    const buildArray = useCallback(() => [num1, num2], [num1, num2]);
-
-    useEffect(() => {
-        console.log(`New array: ${buildArray()}`);
-        setResult(buildArray());
-    }, [buildArray]);
-
-    return (
-        <main className="App">
-            <input type="text" placeholder="input" value={userInput} onChange={(e) => setUserInput(e.target.value)} />
-            <h1>Output: {userInput || "--"}</h1>
-            <p>Result: {JSON.stringify(result)}</p>
-        </main>
-    )
+const getArray = () => {
+  for (let i = 0; i < 1000000000; i++) {
+    //do something expensive
+  }
+  return ['Dave', 'Gray'];
 }
 
-export default App
+function App() {
+  const [userNumber, setUserNumber] = useState('');
+  const [randomInput, setRandomInput] = useState('');
+
+  const fib = useCallback((n) => {
+    return (n <= 1) ? n : fib(n - 1) + fib(n - 2);
+  }, [])
+
+  const fibNumber = useMemo(() => fib(userNumber), [userNumber, fib]);
+
+  const myArray = useMemo(() => getArray(), []);
+
+  useEffect(() => {
+    console.log('New array')
+  }, [myArray])
+
+  return (
+    <main className="App">
+      <label>Fibonacci Sequence:</label>
+      <input type="number" value={userNumber} placeholder="Position" onChange={(e) => setUserNumber(e.target.value)} />
+      <p>Number: {fibNumber || "--"}</p>
+      <br /><br />
+      <label>Random Input:</label>
+      <input type="text" value={randomInput} placeholder="Random Input" onChange={(e) => setRandomInput(e.target.value)} />
+      <p>{randomInput}</p>
+    </main>
+  );
+}
+
+export default App;
